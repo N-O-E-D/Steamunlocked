bfe316550b21134ad12e6bc1325de46d783e3e43
NOED <38118524+N-O-E-D@users.noreply.github.com>
2021-05-16 19:17:51 +0200

    Version 1.0
    Comments in english, text color changed and a major update: overlib implementation (when scrolling the mouse over the link, an image preview of the game will appear. ATTENTION: it may take some time to retrieve the image).

--- script.js
+++ script.js
@@ -1,58 +1,80 @@
 // ==UserScript==
 // @name         Steamunlocked script
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0
 // @description  Script that notices you if there are new games on the site
 // @author       NOED
-// @match        https://steamunlocked.net/all-games
+// @match        https://steamunlocked.net/all-games/
 // @grant        none
+// @require      https://raw.githubusercontent.com/overlib/overlib/master/overlib.js
 // ==/UserScript==
 
+
 window.onload=function() {
+
     'use strict';
 
-    var numprev=window.localStorage.getItem("numprev"); //numero di giochi precedenti all'esecuzione di questo script;
-    var elements=document.getElementsByClassName("blog-content")[0].children[0].children; //lista di elementi (giochi presenti)
-    var numactual=elements.length; //numero di giochi presenti nella pagina appena caricata
-    var pos=document.getElementsByClassName("blog-content")[0]; //banner "All Games (A-Z)"
+    var numprev=window.localStorage.getItem("numprev"); //number of games before the execution of this script;
+    var elements=document.getElementsByClassName("blog-content")[0].children[0].children; //list of elements (all games)
+    var numactual=elements.length; //length of the list (# all games)
+    var pos=document.getElementsByClassName("blog-content")[0]; //"All Games (A-Z)" banner
     console.log("#games before: " + numprev)
     console.log("#games now: " + numactual)
 
     if(numprev!=null){
 
-        var popgamescolumn=document.getElementsByClassName("col-lg-4")[0]; //elemento colonna "popular games"
-        var infocolumn=document.createElement("div"); //elemento colonna "informazioni" da aggiungere per i nuovi giochi inseriti
+        var popgamescolumn=document.getElementsByClassName("col-lg-4")[0]; //"popular games" column element
+        var infocolumn=document.createElement("div"); //new element for new games, to be inserted above "popular games" column
         infocolumn.className="col-lg-4";
         infocolumn.id="infocolumn";
 
-        var listnew=document.createElement("ul"); //lista nuova da inserire. per aggiungere elementi alla lista basta fare listnew.appendchild(li), dove li è una entry da creare
+        var listnew=document.createElement("ul"); //new list to be inserted. to add new elements to the list: listnew.appendchild(li), where li is an entry
         listnew.className="listanuovi";
 
         var nnew=0;
 
-        //e poi cerco
-        for(let element of elements){
-            let link=element.children[0].href; //prende i link
-            let text=element.children[0].text; //prende il testo
-            if(window.localStorage.getItem(text)==null){ //se non si trova nel local storage
-                let entry=document.createElement("li"); //lo aggiungo alla lista info
+        //search:
+        for(let element of elements){ //for each item of the global list (the "all games" one)...
+            let link=element.children[0].href; //...take the link...
+            let text=element.children[0].text; //...take the text...
+            if(window.localStorage.getItem(text)==null){ //if it's not present in the local storage
+                let entry=document.createElement("li"); //...add it to the list
                 let content=document.createElement("a");
                 content.text=text;
                 content.href=link;
+                content.style.color='#C6C6C6';
+                fetch(link).then(function(response){return response.text();})
+                           .then(function(html){
+                               var parser = new DOMParser();
+                               var doc = parser.parseFromString(html, 'text/html');
+                               var overImage = doc.getElementsByClassName("blog-content")[0].querySelector("img");
+                               if(overImage.src.startsWith("http")){
+                                   overImage = "<img src="+overImage.src+">"; //it just works: if src is of type "http...";
+                               }
+                               else if(overImage.src.startsWith("data")){
+                                   overImage = "<img src="+overImage.getAttribute("data-src")+">"; //it just works: if src is of type "data.." (so take data-src)
+                               }
+                               else overImage="cannot retrieve image"; //else show an error
+                               //console.log(overImage);
+                               content.onmouseover = function(){overlib(overImage, WIDTH, 500, LEFT, VAUTO)}; //...and inflate it as overlib (first param of overlib(...) wants an img string element, not the src string
+                               content.onmouseout = function(){nd()};
+                            })
+                           .catch(function (err) {console.warn('Something went wrong.', err);});
                 entry.appendChild(content);
                 listnew.appendChild(entry);
-                nnew++; //incremento il numero di giochi nuovi
+                nnew++; //increment # of new games
             }
         }
-        infocolumn.appendChild(listnew); //listnew contiene tutti i giochi nuovi: verrà utilizzata alla pressione del tasto UPDATE (successivo)
-        popgamescolumn.insertAdjacentElement("beforebegin",infocolumn); //inserisco "informazioni" sopra a "popular games"
+        infocolumn.appendChild(listnew); //listnew contains all new games: useful at the pressure of the UPDATE button  (in future)
+        popgamescolumn.insertAdjacentElement("beforebegin",infocolumn); //add the new list above "popular games"
 
        if(nnew!=0){
-            pos.insertAdjacentHTML("beforeend","<h1>"+nnew+" NUOVI GIOCHI! </h1>");
+            pos.insertAdjacentHTML("beforeend","<h1>"+nnew+" NEW GAMES! </h1>");
         }
-        pos.insertAdjacentHTML("beforeend","<div>"+numactual+" giochi presenti</div>");
-        infocolumn.insertAdjacentHTML("beforebegin",""+nnew+" nuovi giochi");
+        pos.insertAdjacentHTML("beforeend","<div>"+numactual+" games present</div>");
+        infocolumn.insertAdjacentHTML("beforebegin",""+nnew+" new games");
 
+        //now let's handle the local storage update...
         var btn=document.createElement("button");
         btn.innerHTML="UPDATE";
         btn.style.backgroundColor='#000000';
@@ -60,25 +82,25 @@ window.onload=function() {
             btn.style.visibility = "hidden";
         }
         btn.onclick=function(){
-            window.localStorage.setItem("numprev",numactual); //lo setto direttamente nel local storage, poichè appena lo script parte la prima cosa che controlla è questa variabile nel local storage
-            //scelgo di rifare le stesse operaioni fatte per trovare i giochi nuovi, poichè scandire listnew è oneroso. CONTROLLARE IN FUTURO.
+            window.localStorage.setItem("numprev",numactual); //insert it into the local storage -> this script will check this variable every time
+            //same operations as before. CHECK THIS.
             for(let element of elements){
-                let link=element.children[0].href; //prende i link
-                let text=element.children[0].text; //prende il testo
-                if(window.localStorage.getItem(text)==null){ //se non si trova nel local storage
-                    window.localStorage.setItem(text,link); //lo aggiungo al local storage (mentre prima lo aggiungevo alla lista da mostrare a schermo)
+                let link=element.children[0].href; 
+                let text=element.children[0].text; 
+                if(window.localStorage.getItem(text)==null){
+                    window.localStorage.setItem(text,link); //add it into the local storage
                 }
             }
             window.alert("New games added in the local storage!");
             location.reload();
         }
-        listnew.insertAdjacentElement("afterend",btn); //inserisco un button
+        listnew.insertAdjacentElement("afterend",btn); //add a button
      }
-    else{ //se non ci sono le entry nel local storage
-        numprev=numactual;//se il  numero di giochi precedente non c'è, lo calcolo ora. Questo è il momento 0: da ora in poi il codice mostrerà i giochi nuovi
-        let confirm=window.confirm("No entries in the local storage.\nAdd entries?"); //chiedo prima di aggiungere le entry, anche per permettere una eventuale verifica manuale dell'utente (F12->Application->LocalStorage)
-            if(confirm==true){ //se l'utente conferma
-                for(let element of elements){ //le aggiungo
+    else{ //no entries in the local storage = running this script the first time or entries deleted
+        numprev=numactual; //if previous # games is not present, calculate it now.
+        let confirm=window.confirm("No entries in the local storage.\nAdd entries?"); //ask before add entries, also for a manual verification by the user (F12->Application->LocalStorage)
+            if(confirm==true){ // if the user confirms...
+                for(let element of elements){ //...add entries in the local storage
                     let link=element.children[0].href;
                     let text=element.children[0].text;
                     window.localStorage.setItem(text,link);
